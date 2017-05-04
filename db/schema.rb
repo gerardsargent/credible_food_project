422d0e8a31bdd7928efb2dd55a55f514acd5d0ba
Gerard Sargent <ged.sargent@gmail.com>
2017-05-04 14:48:46 +0100

    Added coverslip column to database and updated user form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429192816) do
+ActiveRecord::Schema.define(version: 20170504131545) do
 
   create_table "samples", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20170429192816) do
     t.date     "sample_date"
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
+    t.integer  "coverslip"
   end
 
 end
