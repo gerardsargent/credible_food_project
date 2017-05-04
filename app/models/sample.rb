885e34d39928bcba583cb6272429cf53f83bcbef
Gerard Sargent <ged.sargent@gmail.com>
2017-05-04 13:45:23 +0100

    Set up a simple (test) formula on model using ActiveRecord callback

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,2 +1,8 @@
 class Sample < ApplicationRecord
+
+    before_create :no_bacteria_per_gram_calc
+
+    def no_bacteria_per_gram_calc
+        self.no_bacteria_per_gram = self.bacterial_number + self.actinobacteria
+    end
 end
