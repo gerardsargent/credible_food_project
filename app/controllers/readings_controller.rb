90f394e2c8f1f7c52c625426f9c02a8fb082a901
Ryan Lelliott <lelliott.ryan@gmail.com>
2017-05-30 16:45:02 +0100

    Added an each loop for the readings.
    Each reading will now be listed on the dashboard page.

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -14,13 +14,14 @@ class ReadingsController < ApplicationController
 
   def create
     # render plain: params[:readings].inspect
-    @readings = Reading.new(params[reading_params])
+    readings_params = params.require(:readings).permit(:land_name, :crop_growing, :gps)
+    @readings = Reading.new(readings_params)
     @readings.save
     redirect_to @readings
   end
 
   private def reading_params
-    params.require(:readings).permit(:land_name, :crop_growing, :gps)
+    readings_params = params.require(:readings).permit(:land_name, :crop_growing, :gps)
   end
 
 end
