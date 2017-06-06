56286df9320d55a274ac4115a21c54359b58a311
Ryan Lelliott <lelliott.ryan@gmail.com>
2017-06-06 14:59:57 +0100

    Added broken edit function.
    Will fix after merge.

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -20,14 +20,19 @@ class ReadingsController < ApplicationController
   end
 
   def edit
+    @readings = Reading.find(params[:id])
+  end
+
+  def update
+    @readings = Reading.find(params[:id])
     readings_params = params.require(:readings).permit(:land_name, :crop_growing, :gps)
-    @readings = Reading.new(readings_params)
+    @readings.update(reading_params)
+    redirect_to @readings
   end
 
   def destroy
     @readings = Reading.find(params[:id])
     @readings.destroy
-    #@readings.save
     redirect_to readings_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,12 @@
 Rails.application.routes.draw do
 
-  # resources :readings
-
-  #post 'readings_page', to: 'readings#create'
-
- # get 'readings' => 'readings#readings_page'
-
-  #get 'readings_page', to: 'readings#readings_page' #, as: 'readings'
-
-  #get 'readings/new', to: 'readings#new', as: 'new'
-
-  #get 'readings/show', to: 'readings#show', as: 'show'
-
-  #delete 'page/:id', to: 'readings#destroy'
-  
-  #get '/readings', to: 'readings#readings_page'
-  #post '/readings', to: 'readings#create'
-  #get '/readings/new', to: 'readings#new', as: 'new_page'
-  #get '/readings/:id', to: 'readings#show', as: 'page'
-  #get '/readings/:id/edit', to: 'readings#edit', as: 'edit_page'
-  #patch '/readings/:id', to: 'readings#update'
-  #delete '/readings/:id', to: 'readings#destroy'
+  # get '/readings', to: 'readings#index'
+  # post '/readings', to: 'readings#create'
+  # get '/readings/new', to: 'readings#new', as: 'new_page_page'
+  # get '/readings/:id', to: 'readings#show', as: 'page'
+  # get '/readings/:id/edit', to: 'readings#edit', as: 'edit_page'
+  # patch '/readings/:id', to: 'readings#update'
+  # delete '/readings/:id', to: 'readings#destroy'
 
   resources :readings
   resources :samples
